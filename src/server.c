7c21658ad370080c0f19fb277267969f127c798c
nicolasanjoran <nicolasanjoran@08bb26e5-3090-852d-cc89-35e5d3afa347>
2015-01-10 11:16:00 +0000

    Client management (for connection).

--- src/server.c
+++ src/server.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
@@ -10,6 +11,7 @@
 #define SERVER_PORT 1500
 #define MAX_MSG 1500
 #define MAX_CLIENTS 100
+#define MAX_CLIENTS_digits 3
 #define MAX_CHANNELS 10
 //==========================================================================//
 
@@ -17,37 +19,174 @@
 
 //=========================== GLOBAL VARs & Types ==========================//
 struct channel {
-  int idChannel;
+  int idChannel; // -1 means not specified.
   char* name;
+  int nbClients;
   int clients[MAX_CLIENTS];
 };
 
 struct client{
-  int idClient;
+  int idClient; // -1 means not specified.
   char* name;
   struct sockaddr_in client_addr;
 };
 
 struct client clients[MAX_CLIENTS];
 struct channel channels[MAX_CHANNELS];
+struct sockaddr_in client_addr, server_addr;
+socklen_t addr_len;
+int sd;
+int nbConnectedClients;
 //==========================================================================//
 
 
 //============================ Fonctions ===================================//
-// Return the idChannel
-int CHANNEL_ConnectClient(int client, char* channel);
 
-void CHANNEL_DisconnectClient(int idClient, int channel);
+init()
+{
+	int i;
+	for(i=0 ; i<MAX_CLIENTS ; i++)
+	{
+		clients[i].idClient = -1;
+		clients[i].name = NULL;
+	}
+
+	for(i=0 ; i<MAX_CHANNELS ; i++)
+	{
+		channels[i].idChannel = -1;
+		channels[i].name = NULL;
+		channels[i].nbClients = 0;
+	}
+}
+
+// Return the idChannel
+int CHANNEL_JoinClient(int client, char* channel)
+{
+	return 0;
+}
+
+void CHANNEL_LeaveClient(int idClient, int channel);
 
 // Return the idClient
-int acceptClient(char* client);
+int acceptClient(char* clientName)
+{
+	int i;
+	int id = -1;
+	char uniqueName = 1;
+	int result;
+	if(nbConnectedClients == MAX_CLIENTS)
+	{
+		printf("New client cannot be accepted, MAX_CLIENTS number has already been reached.\n");
+		result = -1;
+	}else{
+		for(i=0 ; i<MAX_CLIENTS ; i++)
+		{
+			if(clients[i].idClient!= -1 && strcmp(clientName, clients[i]) == 0)
+			{
+				uniqueName = 0;
+				break;
+			}
 
-void disconnectClient(int client);
+			if(clients[i].idClient == -1)
+			{
+				id=i;
+			}
+		}
+
+		if(id!=-1 && uniqueName == 1)
+		{
+			clients[id].idClient = id;
+			clients[id].name = clientName;
+			printf("Client %s just connected.\n", clientName);
+			result = id;
+		}
+		else if(id!=-1 && uniqueName == 0)
+		{
+			printf("New client cannot be accepted, specified name already exists.\n");
+			result=-2;
+		}
+	}
+
+	return result;
+}
+
+void disconnectClient(int clientNB)
+{
+	int i;
+
+	if(clientNB < MAX_CLIENTS && clients[clientNB].idClient != -1)
+	{
+		printf("Client %s disconnected.\n", clients[clientNB].name);
+		clients[clientNB].idClient = -1;
+		clients[clientNB].name = NULL;
+	}
+
+	for(i=0 ; i<MAX_CHANNELS ; i++)
+	{
+		CHANNEL_LeaveClient(clientNB, i);
+	}
+}
 
 int transmit(int client, char* message);
 
 void analyzeFrame(char* frame)
 {
+	/*
+	 * This is used for extracting fields from the received frame.
+	 */
+	char** extractedFrame;
+	extractedFrame = (char**)malloc(5*sizeof(char*));
+	char delimiter = 0x01;
+	int idx=0;
+	int i=0;
+	char sum=0;
+	extractedFrame[idx]=strtok(frame, &delimiter); // Transformer le 0x00 en string.
+	for(idx=0 ; idx<4 ; idx++)
+	{
+		extractedFrame[idx+1] = strtok(NULL, &delimiter);
+		if(extractedFrame[idx]==NULL) break;
+
+	}
+	printf("TotalExtracted: %d\n", idx);
+
+	/*
+	 * Check the correctness of the frame (from its Checksum)
+	 */
+	for(i=0 ; i<5 && extractedFrame[i]!=NULL ; i++)
+	{
+		printf("i=%d, rcvd: %s\n", i, extractedFrame[i]);
+	}
+
+
+	/*
+	 * This is to identify which kind of frame it is.
+	 */
+	int strSize = sizeof(extractedFrame[0])/sizeof(char);
+	if(strncmp(extractedFrame[0], "CONNECT", strSize) == 0)
+	{
+		acceptClient(extractedFrame[1]);
+	}
+	else if(strncmp(extractedFrame[0], "JOIN", strSize) == 0)
+	{
+
+	}
+	else if(strncmp(extractedFrame[0], "SAY", strSize) == 0)
+	{
+
+	}
+	else if(strncmp(extractedFrame[0], "LEAVE", strSize) == 0)
+	{
+
+	}
+	else if(strncmp(extractedFrame[0], "DISCONNECT", strSize) == 0)
+	{
+
+	}
+	else if(strncmp(extractedFrame[0], "ACK", strSize) == 0)
+	{
+
+	}
+
 
 }
 
@@ -58,10 +197,11 @@ int printStatus();
 //============================= MAIN =======================================//
 int main(void)
 {
-  int sd, n;
-  socklen_t addr_len;
-  struct sockaddr_in client_addr, server_addr;
+  int n;
   char msgbuf[MAX_MSG];
+
+  init();
+
   // Create socket
   if ((sd = socket(PF_INET, SOCK_DGRAM, 0)) == -1)
   {
@@ -82,13 +222,13 @@ int main(void)
   for (;;)
   {
     addr_len = sizeof(client_addr);
-    n = recvfrom(sd, msgbuf, MAX_MSG, 0,
-        (struct sockaddr *)&client_addr, &addr_len);
+    n = recvfrom(sd, msgbuf, MAX_MSG, 0, (struct sockaddr *)&client_addr, &addr_len);
     if (n == -1)
       perror("recvfrom");
     else {
-      printf("received from %s: %s\n", inet_ntoa(client_addr.sin_addr), msgbuf);
-      analyzeFrame(msgbuf);
+    	printf("received from %s: %s\n", inet_ntoa(client_addr.sin_addr), msgbuf);
+    	analyzeFrame(msgbuf);
+
       //sendto(sd, "OK", sizeof("OK"), 0, (struct sockaddr *)&client_addr, addr_len);
     }
   }
